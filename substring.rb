17b4bdcce4441bc807eae90cd33269368f70fef3
Simon Opheim <simon.opheim@gmail.com>
2015-07-05 16:41:22 +0200

    create method file

---
+++ substring.rb
@@ -0,0 +1,16 @@
+def substrings(words, dictionary)
+    
+    
+end
+
+
+
+
+=begin
+    > dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+    => ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+    > substrings("below", dictionary)
+    => {"below"=>1, "low"=>1}
+    
+=end
